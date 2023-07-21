4e1af43d1f7d2b8274bdb422f23fb33136f9478c
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-07-21 22:01:52 +0530

    Add files via upload

---
+++ src/DarkEntities.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AmitxD\DarkEntities;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class DarkEntities extends PluginBase {
+
+    protected static $instance;
+    private $EntityManager;
+
+    public function onLoad(): void {
+        self::$instance = $this;
+    }
+
+    public function onEnable(): void {
+        $this->EntityManager = new EntityManager();
+        $this->EntityManager->registerEntities();
+    }
+
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        switch ($command->getName()) {
+            case "summonboss":
+                if (count($args) === 1) {
+                    $entityName = strtolower($args[0]);
+                    switch ($entityName) {
+                        case "herobrine":
+                            $this->EntityManager->summonHerobrine($sender);
+                            break;
+                        case "null":
+                            //   $this->EntityManager->summonNull($sender);
+                            break;
+                        case "entity303":
+                            //  $this->EntityManager->summonEntity303($sender);
+                            break;
+                        default:
+                            $sender->sendMessage("§cUsage: /summonboss <Herobrine||Null||Entity303>");
+                            break;
+                    }
+                } else {
+                    $sender->sendMessage("§cUsage: /summonboss <Herobrine||Null||Entity303>");
+                }
+                return true;
+        }
+    }
+
+    public static function getInstance(): self {
+        return self::$instance;
+    }
+}
